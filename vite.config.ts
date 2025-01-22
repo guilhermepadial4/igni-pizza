4620672df7e8febda0b5338144e6cc2084f597e0
Guilherme Padial <guilherme.padial4@gmail.com>
2025-01-22 11:50:05 -0300

    fix: config vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/igni-pizza",
   plugins: [react()],
   resolve: {
     alias: {
